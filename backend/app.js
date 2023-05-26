2f6ac9517c066a372f0b8257f4d73dd0692ed5ed
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2023-05-27 00:21:44 +0300

    fix: исправления после ревью

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 
 const errorsMiddleware = require('./middlewares/errors');
 
-const app = express();
-
-const { PORT = 3000 } = process.env;
-
 // eslint-disable-next-line import/order
 const cors = require('cors');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+app.use(cors());
 
 app.use(express.json());
 
@@ -17,8 +17,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {});
 app.use('/', require('./routes/index'));
 
 app.use(errorsMiddleware);
-
-app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту: ${PORT}`);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -27,9 +27,9 @@ router.use(authMiddleware);
 router.use('/users', userRouter);
 router.use('/cards', cardsRouter);
 
+router.use('/*', (req, res, next) => next(new NotFoundError('404: Not Found.')));
 router.use(errorLogger); // подключаем логгер ошибок
 
-router.use('/*', (req, res, next) => next(new NotFoundError('404: Not Found.')));
 router.use(errors({ message: 'Ошибка валидации данных!' }));
 
 module.exports = router;
